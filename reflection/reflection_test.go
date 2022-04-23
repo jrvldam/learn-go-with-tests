9008b82f0f162a97565e4ef9c73b75568d63e033
jrvldam <jrvldam@gmail.com>
2022-04-23 21:12:56 +0200

    works with maps

--- reflection/reflection_test.go
+++ reflection/reflection_test.go
@@ -66,6 +66,14 @@ func TestWalk(t *testing.T) {
 			},
 			[]string{"London", "Reykjavík"},
 		},
+		{
+			"Maps",
+			map[string]string{
+				"Foo": "Bar",
+				"Baz": "Boz",
+			},
+			[]string{"Bar", "Boz"},
+		},
 	}
 
 	fmt.Println("foo")
