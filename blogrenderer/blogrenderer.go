121dc75acb276954c167371bc138575cc55e209a
jrvldam <jrvldam@gmail.com>
2022-08-15 18:37:39 +0200

    add index

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -4,30 +4,50 @@ import (
 	"embed"
 	"html/template"
 	"io"
+	"strings"
 )
 
 var (
-  //go:embed "templates/*"
-  postTemplates embed.FS
+	//go:embed "templates/*"
+	postTemplates embed.FS
 )
 
 type PostRender struct {
-  templ *template.Template
+	templ *template.Template
 }
 
 func NewPostRender() (*PostRender, error) {
-  templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
-  if err != nil {
-    return nil, err
-  }
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
+	if err != nil {
+		return nil, err
+	}
 
-  return &PostRender{templ: templ}, nil
+	return &PostRender{templ: templ}, nil
 }
 
 func (r *PostRender) Render(w io.Writer, p Post) error {
-  if err := r.templ.Execute(w, p); err != nil {
-    return err
-  }
+	if err := r.templ.Execute(w, p); err != nil {
+		return err
+	}
 
-  return nil
+	return nil
+}
+
+func (r *PostRender) RenderIndex(w io.Writer, posts []Post) error {
+	indexTemplate := `<ol>{{range .}}<li><a href="/post/{{sanitiseTitle .Title}}">{{.Title}}</a></li>{{end}}</ol>`
+
+	templ, err := template.New("index").Funcs(template.FuncMap{
+		"sanitiseTitle": func(title string) string {
+			return strings.ToLower(strings.Replace(title, " ", "-", -1))
+		},
+	}).Parse(indexTemplate)
+	if err != nil {
+		return err
+	}
+
+	if err = templ.Execute(w, posts); err != nil {
+		return err
+	}
+
+	return nil
 }
