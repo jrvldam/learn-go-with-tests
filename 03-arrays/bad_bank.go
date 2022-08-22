16850a44ce3e668da56ec69326551c894dba1333
jrvldam <jrvldam@gmail.com>
2022-08-22 13:18:34 +0200

    extending the bank

--- 03-arrays/bad_bank.go
+++ 03-arrays/bad_bank.go
@@ -6,6 +6,31 @@ type Transaction struct {
 	Sum  float64
 }
 
+func NewTransaction(from, to Account, sum float64) Transaction {
+	return Transaction{From: from.Name, To: to.Name, Sum: sum}
+}
+
+type Account struct {
+	Name    string
+	Balance float64
+}
+
+func NewBalanceFor(account Account, transactions []Transaction) Account {
+	return Reduce(transactions, applyTransaction, account)
+}
+
+func applyTransaction(account Account, transaction Transaction) Account {
+	if transaction.From == account.Name {
+		account.Balance -= transaction.Sum
+	}
+
+	if transaction.To == account.Name {
+		account.Balance += transaction.Sum
+	}
+
+	return account
+}
+
 func BalanceFor(transactions []Transaction, name string) float64 {
 	adjustBalance := func(currentBalance float64, transaction Transaction) float64 {
 		if transaction.From == name {
--- 03-arrays/bad_bank_test.go
+++ 03-arrays/bad_bank_test.go
@@ -3,20 +3,19 @@ package arrays
 import "testing"
 
 func TestBadBank(t *testing.T) {
+	amaya := Account{Name: "Amaya", Balance: 100}
+	rodrigo := Account{Name: "Rodrigo", Balance: 75}
+	nayra := Account{Name: "Nayra", Balance: 200}
 	transactions := []Transaction{
-		{
-			From: "Rodrigo",
-			To:   "Amaya",
-			Sum:  100,
-		},
-		{
-			From: "Nayra",
-			To:   "Rodrigo",
-			Sum:  25,
-		},
+		NewTransaction(rodrigo, amaya, 100),
+		NewTransaction(nayra, rodrigo, 25),
 	}
 
-	AssertEqual(t, BalanceFor(transactions, "Amaya"), 100)
-	AssertEqual(t, BalanceFor(transactions, "Rodrigo"), -75)
-	AssertEqual(t, BalanceFor(transactions, "Nayra"), -25)
+	newBalanceFor := func(account Account) float64 {
+		return NewBalanceFor(account, transactions).Balance
+	}
+
+	AssertEqual(t, newBalanceFor(amaya), 200)
+	AssertEqual(t, newBalanceFor(rodrigo), 0)
+	AssertEqual(t, newBalanceFor(nayra), 175)
 }
