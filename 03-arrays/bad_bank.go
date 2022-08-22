713e17d69010b63f02d5dc9be1a077e1a46dede3
jrvldam <jrvldam@gmail.com>
2022-08-22 13:29:03 +0200

    remove death code

--- 03-arrays/bad_bank.go
+++ 03-arrays/bad_bank.go
@@ -30,19 +30,3 @@ func applyTransaction(account Account, transaction Transaction) Account {
 
 	return account
 }
-
-func BalanceFor(transactions []Transaction, name string) float64 {
-	adjustBalance := func(currentBalance float64, transaction Transaction) float64 {
-		if transaction.From == name {
-			return currentBalance - transaction.Sum
-		}
-
-		if transaction.To == name {
-			return currentBalance + transaction.Sum
-		}
-
-		return currentBalance
-	}
-
-	return Reduce(transactions, adjustBalance, 0.0)
-}
