2f5a589250d02629739e60d27173c0995db0f437
jrvldam <jrvldam@gmail.com>
2022-08-23 20:50:31 +0200

    file system store

---
+++ 21-io/file_system_store_test.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestFileSystemStore(t *testing.T) {
+	t.Run("league from a reader", func(t *testing.T) {
+		database := strings.NewReader(`
+      [{"Name": "Nayra", "Wins": 10},
+      {"Name": "Amaya", "Wins": 33}]
+    `)
+		store := FileSystemPlayerStore{database}
+
+		got := store.GetLeague()
+		want := []Player{
+			{"Nayra", 10},
+			{"Amaya", 33},
+		}
+
+		assertLeague(t, got, want)
+	})
+}
