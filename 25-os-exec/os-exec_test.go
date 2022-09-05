c6cd99931da1b9419d86e51a2322fb6828823d6c
jrvldam <jrvldam@gmail.com>
2022-09-05 22:45:48 +0200

    os exect seperate concerns to testability

---
+++ 25-os-exec/os-exec_test.go
@@ -0,0 +1,57 @@
+package osexec
+
+import (
+	"bytes"
+	"encoding/xml"
+	"io"
+	"io/ioutil"
+	"os/exec"
+	"strings"
+	"testing"
+)
+
+type Payload struct {
+	Message string `xml:"message"`
+}
+
+func GetData(data io.Reader) string {
+	var payload Payload
+	xml.NewDecoder(data).Decode(&payload)
+
+	return strings.ToUpper(payload.Message)
+}
+
+func GetXmlFromCommand() io.Reader {
+	cmd := exec.Command("cat", "msg.xml")
+	out, _ := cmd.StdoutPipe()
+
+	cmd.Start()
+	data, _ := ioutil.ReadAll(out)
+	cmd.Wait()
+
+	return bytes.NewReader(data)
+}
+
+func TestGetData(t *testing.T) {
+	got := GetData(GetXmlFromCommand())
+	want := "HAPPY NEW YEAR!"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
+
+func TestGetDataUnit(t *testing.T) {
+	input := strings.NewReader(`
+<payload>
+    <message>Cats are the best animal</message>
+</payload>
+    `)
+
+	got := GetData(input)
+	want := "CATS ARE THE BEST ANIMAL"
+
+	if got != want {
+		t.Errorf("got %q, want %q", got, want)
+	}
+}
